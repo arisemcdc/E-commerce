7837d4dc89c98a0570a386dfa9ea6fb78bc591dc
Arisemcdc <arisemcdc1@gmail.com>
2020-11-08 00:17:19 +0300

    add StoreProductFragment

--- app/src/main/java/com/example/e_commerce/ui/StoreFront/StoreProductFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/StoreFront/StoreProductFragment.kt
@@ -5,17 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavArgs
 import com.example.e_commerce.Data.Product
+import com.example.e_commerce.Data.Repository
+import com.example.e_commerce.EcommerceApp
 import com.example.e_commerce.R
+import kotlinx.android.synthetic.main.fragment_store_product.view.*
 
 
 class StoreProductFragment(val product: Product) : Fragment() {
-
+    lateinit var root: View
+    private val repository = EcommerceApp.repository
+    val products = repository.products
+    //val args = StoreProductFragmentArgs by NavArgs
+    private var _productId: String? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store_product, container, false)
+       // setProductId(args)
+        root = inflater.inflate(R.layout.fragment_store_product,  container, false)
+
+       // Repository.products.
+        return root
+    }
+    private fun setProductId(id: String?){
+        _productId = id
+        if (id != null){
+            val product = products.value?.find { _productId == it.id }
+            product?.let {
+                setProductsValues(it)
+            }
+        }
+    }
+    private fun setProductsValues(product: Product) {
+        root.nametextView.setText(product.name)
+        root.pricetextViewValue.setText(product.price.toString())
+        root.amounttextViewValue.setText(product.amount.toString())
     }
 }
